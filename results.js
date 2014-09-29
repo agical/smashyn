c447e9db5b724e1b85cdf0f31613667faa478e49
Daniel Brolund <daniel.brolund@agical.com>
2014-09-29 21:56:56 +0200

    Tidied up reporting

--- results.js
+++ results.js
@@ -1,22 +1,22 @@
 define([], function() {
     return { 
         module_passed: function(obj) {
-            console.log("- ", obj);
+            console.log(obj.module, ' OK!');
         },
         scenario_passed: function(obj) {
-            console.log("    - ", obj);
+            console.log("  ", obj.scenario);
         },
         check_passed: function(obj) {
-            console.log("      -", obj);
+            console.log("    ", obj.check);
         },
         module_failed: function(obj) {
-            console.log("* ", obj);
+            console.log("**", obj.module, ' FAILED: ', obj.error);
         },
         scenario_failed: function(obj) {
-            console.log("    * ", obj);
+            console.log("  **", obj.scenario);
         },
         check_failed: function(obj) {
-            console.log("      * ", obj);
+            console.log("    **", obj.check);
         },
         error: function(obj) {
             console.log("*#@:", obj);
--- test/multiplication_test.js
+++ test/multiplication_test.js
@@ -1,6 +1,9 @@
 define(['multiplication'], function(multiplication) {
     return {
-        test: { 'Multiply': function(eq) {return eq(multiplication(2, 3), 6)} },
+        test: { 'Multiply': function(eq) {
+            return eq(multiplication(2, 3), 6, "Positive numbers") 
+                && eq(multiplication(-2,3), -6, "One negative number");
+        }},
         result: multiplication
     }
 });

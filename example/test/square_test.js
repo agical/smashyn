abd6e1e994b70927df64a4fc6a8a25931cd56aa5
Daniel Brolund <daniel.brolund@agical.com>
2014-10-05 10:18:31 +0200

    Happy path working

--- example/test/square_test.js
+++ example/test/square_test.js
@@ -4,7 +4,7 @@ define(['square', 'smashyn', 'multiplication_test'], function( square, smashyn) 
             module: 'Square',
             scenarios: {
                 'Square': function(c) {
-                    return c.eq(square(3), 9, "Square positive number");
+                    return c.eq(square(3), 8, "Square positive number");
                 },
             },
             result: square
